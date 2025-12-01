3f2a140eb5826b061a1814f73e1cf163e36fe457
CesarOrt <lopezortigozacesarramon@gmail.com>
2025-12-01 17:09:53 -0600

    Practica 13 terminada con Composer

--- practicas/p12/product_app/backend/product-edit.php
+++ practicas/p12/product_app/backend/product-edit.php
@@ -1,42 +1,13 @@
 <?php
-    include_once __DIR__.'/database.php';
+use TECWEB\MYAPI\Update\Update;
+require_once __DIR__ . '/myapi/vendor/autoload.php';
 
-    $producto = file_get_contents('php://input');
-    
-    if(!empty($producto)) {
-        $jsonOBJ = json_decode($producto);
-        
-        $id = $jsonOBJ->id;
-        $nombre = mysqli_real_escape_string($conexion, $jsonOBJ->nombre);
-        $marca = mysqli_real_escape_string($conexion, $jsonOBJ->marca);
-        $modelo = mysqli_real_escape_string($conexion, $jsonOBJ->modelo);
-        $precio = $jsonOBJ->precio;
-        $detalles = mysqli_real_escape_string($conexion, $jsonOBJ->detalles);
-        $cantidad = $jsonOBJ->cantidad;
-        $imagen = mysqli_real_escape_string($conexion, $jsonOBJ->imagen);
-        
-        $query = "UPDATE productos SET 
-                  nombre = '{$nombre}',
-                  marca = '{$marca}',
-                  modelo = '{$modelo}',
-                  precio = {$precio},
-                  detalles = '{$detalles}',
-                  cantidad = {$cantidad},
-                  imagen = '{$imagen}'
-                  WHERE id = {$id}";
-        
-        if($conexion->query($query)) {
-            echo json_encode(array(
-                'status' => 'success',
-                'message' => 'Producto modificado exitosamente'
-            ));
-        } else {
-            echo json_encode(array(
-                'status' => 'error',
-                'message' => 'Error al modificar: ' . mysqli_error($conexion)
-            ));
-        }
-        
-        $conexion->close();
-    }
+$producto = file_get_contents('php://input');
+
+if(!empty($producto)) {
+    $jsonOBJ = json_decode($producto);
+    $update = new Update('marketzone');
+    $update->edit($jsonOBJ);
+    echo $update->getData();
+}
 ?>
--- practicas/p12/product_app/backend/product-single.php
+++ practicas/p12/product_app/backend/product-single.php
@@ -1,18 +1,11 @@
 <?php
-    include_once __DIR__.'/database.php';
+use TECWEB\MYAPI\Read\Read;
+require_once __DIR__ . '/myapi/vendor/autoload.php';
 
-    if(isset($_GET['id'])) {
-        $id = $_GET['id'];
-        
-        $query = "SELECT * FROM productos WHERE id = {$id}";
-        $result = $conexion->query($query);
-        
-        if($result) {
-            $producto = $result->fetch_assoc();
-            echo json_encode($producto);
-            $result->free();
-        }
-        
-        $conexion->close();
-    }
+if(isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $read = new Read('marketzone');
+    $read->single($id);
+    echo $read->getData();
+}
 ?>
